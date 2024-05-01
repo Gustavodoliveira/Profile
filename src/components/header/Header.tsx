99cde43fcbcf93eab46cb338dbe8d5915b4c4bfb
Gustavo <gustavod.oliveira@outlook.com>
2024-04-30 22:18:27 -0300

    Feat: dowload cv complete

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -11,11 +11,8 @@ import {
 import Link from 'next/link';
 import Button from '../button/Button';
 import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
-import {
-  BiArrowFromBottom,
-  BiArrowToBottom,
-  BiDownArrow,
-} from 'react-icons/bi';
+import { BiDownArrow } from 'react-icons/bi';
+import file from '../../../public/images/frequency-wave-7776034_1280.jpg';
 
 const Header = () => {
   return (
@@ -56,7 +53,9 @@ const Header = () => {
         <div className="image">
           <h2>image</h2>
         </div>
-        <button>dowload cv</button>
+        <a href={file.src} download={'file.doc'}>
+          dowload cv
+        </a>
       </HeaderImageAndCvContainer>
       <BiDownArrow className="Arrow-on-bottom" />
     </HeaderContainer>
