3f038682091caaafa5ab781f969d7e071e2b047f
Gustavo <gustavod.oliveira@outlook.com>
2024-08-25 13:14:03 -0300

    clear past

--- styledComponents.d.ts
+++
@@ -1,8 +0,0 @@
-import 'styled-components';
-import Theme from '@/app/Variables';
-
-type ITheme = typeof Theme
-
-declare module 'style-components' {
-  export interface DefaultTheme extends  ITheme{}
-};
