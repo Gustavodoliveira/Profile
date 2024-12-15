f3d938a2b43fddc4a5f8412fa7e77addd9042357
Gustavo <gustavod.oliveira@outlook.com>
2024-12-15 20:03:48 -0300

    feat: first page mobile complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,38 @@
+import Image from "next/image";
 import styles from "./page.module.css";
+import img from "../../public/19362653.jpg";
+import Btn from "@/components/btn/Btn";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <h1>Ola</h1>
-    </div>
+    <main className={styles.main}>
+      <header className={styles.main_header}>
+        <h1>Desenvolvedor Full Stack</h1>
+        <Image
+          src={img}
+          alt="Desenvolvedor"
+          width={350}
+          height={300}
+          className={styles.image_center}
+        />
+      </header>
+      <section className={styles.main_contacts}>
+        <Btn
+          className="btn_linkedin"
+          link=""
+          text="Linkedin"
+          Icon={FaLinkedin}
+          classIcon="icon_linkedin"
+        />
+        <Btn
+          className="btn_github"
+          link=""
+          text="GitHub"
+          Icon={FaGithub}
+          classIcon="icon_github"
+        />
+      </section>
+    </main>
   );
 }
---
+++ src/components/btn/Btn.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./Btn.css";
+import { IconType } from "react-icons";
+
+type props = {
+  className: string;
+  link: string;
+  text: string;
+  Icon?: IconType;
+  classIcon: string;
+};
+
+const Btn = ({ className, link, text, Icon, classIcon }: props) => {
+  return (
+    <a href={link} target="_blank" className={`btn ${className}`}>
+      {Icon && <Icon className={`icon ${classIcon}`} />}
+      {text}
+    </a>
+  );
+};
+
+export default Btn;
